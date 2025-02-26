7c1097f85bffafa6946b0d22c934f248b3330828
ryu8655-alt <suzukiriyuuji@gmail.com>
2025-02-26 12:08:36 +0900

    SceneControllerの追加・GameManagerの更新

--- Assets/SuzukiRyuuji/Script/GameManager.cs
+++ Assets/SuzukiRyuuji/Script/GameManager.cs
@@ -10,8 +10,11 @@ using System;
 public enum GameState
 {
     Title,
-    PlayerTurn,
-    EnemyTurn,
+    InGame,
+    PlayerTurn,//プレイヤーのターン
+    EnemyTurn,//エネミーのターン
+    GameClear,
+    StageOUT,//Playerがステージ外に出た場合
     Pause,
     GameOver,
     Result
@@ -21,14 +24,6 @@ public enum GameState
 
 public class GameManager : MonoBehaviour
 {
-    private const string TITLE_SCENE_NAME = "TestTitle";
-    private const string STAGE1_SCENE_NAME = "TestStage1";
-    private const string STAGE2_SCENE_NAME = "TestStage2";
-    private const string RESULT_SCENE_NAME = "TestResult";
-
-    //余力があればOptionシーンを実装する
-    private const string OPTION_SCENE_NAME = "TestOption";
-
     public event Action<GameState> OnGameStateChanged;
 
 
@@ -50,7 +45,7 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            Destroy(this);
+            Destroy(gameObject);
         }
     }
 
@@ -59,23 +54,28 @@ public class GameManager : MonoBehaviour
     void Start()
     {
 
-        //初期状態として必ずTitlシーンをロードする
-        if (SceneManager.GetActiveScene().name != TITLE_SCENE_NAME)
-        {
-            LoadScene(TITLE_SCENE_NAME);
-        }
+     
 
-        _playerScripts = FindAnyObjectByType<Player_Scripts>();
-        //kari
-        currentState = GameState.PlayerTurn;
+        //_playerScripts = FindAnyObjectByType<Player_Scripts>();
+        ////kari
+        //currentState = GameState.PlayerTurn;
+
+        if(currentState == GameState.Title)
+        {
+            SceneController.Instance.LoadTitleScene();
+        }
     }
 
     void Update()
     {
-     if(Input.GetKeyDown(KeyCode.Space))
+        //動作確認用
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            NextLoadScene();
+            SceneController.Instance.LoadNextScene();
         }
+
+
+
         if(currentState == GameState.PlayerTurn && _playerScripts.CheckPlayerEnd())
         {
             Debug.Log("StateChange EnemyTurn");
@@ -90,114 +90,5 @@ public class GameManager : MonoBehaviour
             OnGameStateChanged(GameState.PlayerTurn);
         }
     }
-
-
-    /// <summary>
-    /// 引数で指定されたシーンをロードする
-    /// </summary>
-    /// <param name="sceneName"> </param>
-    private void LoadScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-    }
-
-
-    /// <summary>
-    /// 現在のシーンを参照して対応する次のシーンをロードする
-    /// </summary>
-    public void NextLoadScene()
-    {
-        string currentSceneName = SceneManager.GetActiveScene().name;
-
-        //テスト用に順番でシーンが遷移するようにしている
-        //実際のゲームでは、条件に応じて遷移するシーンを変更するように改修を行う
-
-        if (currentSceneName == TITLE_SCENE_NAME)
-        {
-            toStage1Scene();
-        }
-        else if (currentSceneName == STAGE1_SCENE_NAME)
-        {
-            toStage2Scene();
-        }
-        else if (currentSceneName == STAGE2_SCENE_NAME)
-        {
-            toResultScene();
-        }
-        else if (currentSceneName == RESULT_SCENE_NAME)
-        {
-            toTitleScene();
-        }
-    }
-
-
-    //タイトルシーンへの遷移メソッド
-    public void toTitleScene()
-    {
-        //シーンに対してnullチェックを行う
-        if (SceneManager.GetSceneByName(TITLE_SCENE_NAME) != null)
-        {
-            LoadScene(TITLE_SCENE_NAME);
-        }
-        else
-        {
-          Debug.Log("TestTitleシーンが存在しません");
-        }
-    }
-
-    //ステージ1シーンへの遷移メソッド
-    public void toStage1Scene()
-    {
-        if (SceneManager.GetSceneByName(STAGE1_SCENE_NAME) != null)
-        {
-            LoadScene(STAGE1_SCENE_NAME);
-        }
-        else
-        {
-           Debug.Log("TestStage1シーンが存在しません");
-        }
-    }
-
-    //ステージ2シーンへの遷移メソッド
-    public void toStage2Scene()
-    {
-        if (SceneManager.GetSceneByName(STAGE2_SCENE_NAME) != null)
-        {
-            LoadScene(STAGE2_SCENE_NAME);
-        }
-        else
-        {
-            Debug.Log("TestStage2シーンが存在しません");
-        }
-    }
-
-    //リザルトシーンへの遷移メソッド
-    public void toResultScene()
-    {
-        if (SceneManager.GetSceneByName(RESULT_SCENE_NAME) != null)
-        {
-            LoadScene(RESULT_SCENE_NAME);
-        }
-        else
-        {
-            Debug.Log("TestResultシーンが存在しません");
-        }
-    }
-
-    public void tiOptionScene()
-    {
-
-       if (SceneManager.GetSceneByName(OPTION_SCENE_NAME) != null)
-        {
-            LoadScene(OPTION_SCENE_NAME);
-        }
-        else
-        {
-            Debug.Log("TestOptionシーンが存在しません");
-        }
-    }
-
-
-
 
 }
---
+++ Assets/SuzukiRyuuji/Script/SceneController.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+//シーン遷移の管理を行うManagerクラス
+
+//追加する内容　＝　シーンのnullチェック機能を追加する
+
+
+public class SceneController : MonoBehaviour
+{
+
+
+   // シーンに関してinspector上で設定を行うための変数に変更を行いたい(方法を考案中)
+   //→Scene事態を格納する事ができるらしい  とりあえずはファイル名を格納する形で実装を行う
+    [Header("シーンの設定")]
+    [SerializeField,Header("タイトルシーンファイル名")]
+    private string _titleSceneName ;
+
+    [SerializeField,Header("ステージ1シーンファイル名")]
+    private string _stage1SceneName ;
+
+    [SerializeField,Header("ステージ2シーンファイル名")]
+    private string _stage2SceneName ;
+
+    [SerializeField,Header("リザルトシーンファイル名")]
+    private string _resultSceneName ;
+
+    [SerializeField,Header("Optionシーンファイル名")]
+    private string _optionSceneName ;
+
+
+    //SceneControllerのインスタンスを格納する変数
+    public static SceneController Instance { get; private set; }
+
+    //SceneControllerのインスタンスが存在するかどうかを返すプロパティ
+    private void Awake()
+    {
+        if(Instance == null)//インスタンスが存在しない場合
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    /// <summary>
+    /// 引数で指定されたシーン名のシーンの読み込みを行う
+    /// </summary>
+    /// <param name="sceneName"></param>
+    private void LoadScene(string sceneName)
+    {
+        //引数のシーン名がビルド設定されたシーンに存在しているか判定
+        if (IsSceneExist(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            //存在しない場合には、対象のシーンが存在しない旨をデバッグログで表示
+            Debug.LogWarning($"シーン名{sceneName}は存在しません");
+           
+        }
+    }
+
+    /// <summary>
+    /// 引数のシーン名がビルド設定されたシーンに存在しているかを判定する
+    /// </summary>
+    /// <param name="sceneName"></param>
+    /// <returns></returns>
+    private bool IsSceneExist(string sceneName)
+    {
+
+        for(int i = 0; i < SceneManager.sceneCountInBuildSettings; ++i)
+        {
+            //ビルド設定されているシーンのパスを取得
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+
+            //パスから拡張子を除いたシーン名を取得
+            string sceneNameInBuild = System.IO.Path.GetFileNameWithoutExtension(scenePath);
+
+            //引数のシーン名とビルド設定されたシーン名が一致しているかを判定
+            if(sceneNameInBuild == sceneName)
+            {
+                return true;
+            }
+          
+        }
+        return false;
+    }
+   
+
+
+    //タイトルシーンへの遷移
+    public void LoadTitleScene()
+    {
+        LoadScene(_titleSceneName);
+    }
+
+
+    //ステージ1シーンへの遷移
+    public void LoadStage1Scene()
+    { 
+     LoadScene(_stage1SceneName);
+    }
+
+
+    //ステージ2シーンへの遷移
+    public void LoadStage2Scene()
+    {
+        LoadScene(_stage2SceneName);
+    }
+
+    //リザルトシーンへの遷移
+    public void LoadResultScene()
+    {
+        LoadScene(_resultSceneName);
+    }
+
+    //Optionシーンへの遷移
+    public void LoadOptionScene()
+    {
+        LoadScene(_optionSceneName);
+    }
+
+
+    //デバッグ用シーン遷移メソッド
+    public void LoadNextScene()
+    {
+       //現在のシーン名を取得
+       string currentSceneName = SceneManager.GetActiveScene().name;
+
+        if(currentSceneName == _titleSceneName)
+        {
+            LoadStage1Scene();
+        }
+        else if(currentSceneName == _stage1SceneName)
+        {
+            LoadStage2Scene();
+        }
+        else if(currentSceneName == _stage2SceneName)
+        {
+            LoadResultScene();
+        }
+        else if(currentSceneName == _resultSceneName)
+        {
+            LoadTitleScene();
+        }
+    }
+
+
+
+}
