0b02f2c5cdb60be2154afa73f6207481a82d65ec
O-Haruki <134474921+O-Haruki@users.noreply.github.com>
2025-02-26 10:23:49 +0900

    AudioManger修正

--- Assets/Okamura/AudioManager.cs
+++ Assets/Okamura/AudioManager.cs
@@ -5,9 +5,11 @@ public struct AudioData
 {
     public AudioClip AudioClip;
     public float VolumeIndex;
-    [Range(0.8f, 1.2f)] public float Pitch;
+    public float MaxPitch;
+    public float MinPitch;
 }
 
+[RequireComponent(typeof(AudioSource))]
 [RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
@@ -15,8 +17,11 @@ public class AudioManager : MonoBehaviour
     public static AudioManager Instance => _instance;
 
     public float GlobalVolume { get; set; }
-    [SerializeField] AudioData[] _audioData;
-    AudioSource _audioSource;
+    [SerializeField] AudioData[] _audioDataBGM;
+    [SerializeField] AudioData[] _audioDataSE;
+    AudioSource _audioSourceBGM;
+    AudioSource _audioSourceSE;
+
     private void Awake()
     {
         if (_instance != null)
@@ -28,35 +33,51 @@ public class AudioManager : MonoBehaviour
             _instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        _audioSource = GetComponent<AudioSource>();
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        _audioSourceBGM = audioSources[0];
+        _audioSourceSE = audioSources[1];
     }
 
-    public void PlayClip(int clipIndex)
+    public void PlayClipBGM(int clipIndex)
     {
-        if (_audioData.Length >= clipIndex)
+        if (_audioDataBGM.Length >= clipIndex)
+        {
+            Debug.LogWarning("this bgm index not exist");
+        }
+        else
+        {
+            _audioSourceBGM.clip = _audioDataBGM[clipIndex].AudioClip;
+            _audioSourceBGM.volume = _audioDataBGM[clipIndex].VolumeIndex * GlobalVolume;
+            _audioSourceBGM.Play();
+        }
+    }
+
+    public void PlayClipSE(int clipIndex)
+    {
+        if (_audioDataSE.Length >= clipIndex)
+        {
+            Debug.LogWarning("this se index not exist");
+        }
+        else
+        {
+            _audioSourceSE.clip = _audioDataBGM[clipIndex].AudioClip;
+            _audioSourceSE.volume = _audioDataBGM[clipIndex].VolumeIndex * GlobalVolume;
+            _audioSourceSE.Play();
+        }
+    }
+
+    public void PlayClipPitchedSE(int clipIndex)
+    {
+        if (_audioDataSE.Length >= clipIndex)
         {
             Debug.LogWarning("this index isnt exist");
         }
         else
         {
-            _audioSource.clip = _audioData[clipIndex].AudioClip;
-            _audioSource.volume = _audioData[clipIndex].VolumeIndex * GlobalVolume;
-            _audioSource.Play();
-        }
-    }
-
-    public void PlayClipPitched(int clipIndex)
-    {
-        if (_audioData.Length >= clipIndex)
-        {
-            Debug.LogWarning("this index isnt exist");
-        }
-        else
-        {
-            _audioSource.clip = _audioData[clipIndex].AudioClip;
-            _audioSource.volume = _audioData[clipIndex].VolumeIndex * GlobalVolume;
-            _audioSource.pitch = _audioData[clipIndex].Pitch;
-            _audioSource.Play();
+            _audioSourceSE.clip = _audioDataSE[clipIndex].AudioClip;
+            _audioSourceSE.volume = _audioDataSE[clipIndex].VolumeIndex * GlobalVolume;
+            _audioSourceSE.pitch = UnityEngine.Random.Range(_audioDataSE[clipIndex].MinPitch, _audioDataSE[clipIndex].MaxPitch);
+            _audioSourceSE.Play();
         }
     }
 }
